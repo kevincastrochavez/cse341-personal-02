799cc59463e39f3d317f85604b9fbc2de512e32d
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-27 15:32:33 -0600

    modified function in controller to get all the contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
-router.get('/', contactsController.getContacts);
+router.get('/', contactsController.getAllContacts);
 
 module.exports = router;
