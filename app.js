bd3e05ae2457ea12f731688721bc293993a24558
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-27 11:16:55 -0600

    created web server and connected with mongodb to the users database and contacts collection

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongodb = require('./db/connect');
+const contactsRoutes = require('./routes/contacts');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use('/', contactsRoutes);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
